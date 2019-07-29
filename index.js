9524a5fbe9ade003efa11b480002f860019be265
Zelop <roberto@zelop.net>
2019-07-29 17:28:29 +0200

    Ver0.2 More intuitive config and simpler index.js

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const request = require('request');
 const uuidv4 = require('uuid/v4');
 const util = require('util');
 
-function enToRu(message){
+// How does it work for now?
+/*
+Just go to config.json.
+First channel in channellist will translate to second channel and viceversa. Same with 3rd and 4th channel.
+First language in languages is the language you want THE FIRST CHANNEL TO TRANSLATE INTO. Second for the second channel.
+Origin language isn't needed, Azure api will autodetect it.
+*/
+
+function isOdd(num) { return (num % 2);}
+
+function translateMsg(message, lang, channel){
 	let options = {
 			method: 'POST',
 			baseUrl: 'https://api.cognitive.microsofttranslator.com/',
 			url: 'translate',
 			qs: {
 				'api-version': '3.0',
-				'to': ['ru']
+				'to': [lang]
 			},
 			headers: {
 				'Ocp-Apim-Subscription-Key': config.azurekey,
@@ -34,7 +44,7 @@ function enToRu(message){
 		var abc = '"text": "'
 		var tmp = resp.split(abc);
 		var tpm = tmp[1].split('",');
-		console.log(util.inspect(tpm[0]));
+		//console.log(util.inspect(tpm[0]));
 
 		var usernick = message.author.username;
 		if (message.member.nickname) usernick = message.member.nickname;
@@ -43,72 +53,32 @@ function enToRu(message){
 			.setAuthor(usernick, message.author.avatarURL)
 			.setDescription(tpm[0])
 			.setTimestamp()
-			.setFooter('English? -> Russian');
+			.setFooter(`Translated to ${lang}`);
 
-		client.channels.get(config.russianchannel).send(niceEmbed);
+		client.channels.get(channel).send(niceEmbed);
 		});
-}
-
-function ruToEn (message){
-	let optionsru = {
-			method: 'POST',
-			baseUrl: 'https://api.cognitive.microsofttranslator.com/',
-			url: 'translate',
-			qs: {
-				'api-version': '3.0',
-				'to': ['en']
-			},
-			headers: {
-				'Ocp-Apim-Subscription-Key': config.azurekey,
-				'Content-type': 'application/json',
-				'X-ClientTraceId': uuidv4().toString()
-			},
-			body: [{
-						'text': message.content
-			}],
-			json: true,
-	};
-
-	request(optionsru, function(err, res, body){
-		//console.log(util.inspect(body));
-		if(err) {
-			console.log(err);
-			return;
-		}
-		var resp = JSON.stringify(body, null, 4);
-		var abc = '"text": "'
-		var tmp = resp.split(abc);
-		var tpm = tmp[1].split('",');
-		console.log(util.inspect(tpm[0]));
-
-		var usernick = message.author.username;
-		if (message.member.nickname) usernick = message.member.nickname;
-		const niceEmbed = new Discord.RichEmbed()
-			.setColor('#0099ff')
-			.setAuthor(usernick, message.author.avatarURL)
-			.setDescription(tpm[0])
-			.setTimestamp()
-			.setFooter('Russian? -> English');
-
-		client.channels.get(config.englishchannel).send(niceEmbed);
-	});
-
 }
 
 
 client.once('ready', () => {
-	console.log('Ready!');
+	if (config.channellist.length < '2' || isOdd(config.channellist.length))
+	{
+		console.log(`Error! Not enough channels (Channels = ${config.channellist.length}) in config.json. Bot won't work properly (if at all)`);
+		return;
+	} else console.log('Ready!');
 });
 client.on('message', message => {
   if (message.author.bot) return;
-	if (message.channel.id === config.englishchannel) {
-		console.log(message.channel.id);
-		enToRu(message);
-	} else if (message.channel.id === config.russianchannel) {
-		ruToEn(message);
+	var i = 0;
+	for (i = 0; i < config.channellist.length; i++){
+		if (message.channel.id === config.channellist[i]) {
+				var z = i;
+				if (isOdd(i)) {	z--; } else z++;
+				translateMsg(message, config.languages[i], config.channellist[z]);
+				return;	// No need to stay on the loop
+		}
 	}
 });
-
 
 
 client.login(config.token);
